357e0be175226d47a2093f8625c8ace4ba38d31f
C0B23037 <c0b230376b@edu.teu.ac.jp>
2024-09-24 17:31:09 +0900

    練習問題

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -20,20 +20,23 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()#8_3
+        dx,dy=0,0
         if key_lst [pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            dy=-1
         if key_lst [pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
-        if key_lst [pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            dy=1
         if key_lst [pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            dx=2
+        else:
+            dx=-1
+
+        kk_rct.move_ip(dx,dy)
         x=-(tmr%3200)
         screen.blit(bg_img,[x,0])
         screen.blit(bg_img2,[x+1600,0])
         screen.blit(bg_img,[x+3200,0])
         screen.blit(bg_img2,[x+4800,0])
-        #screen.blit(kk_img, [300, 200])4
+        #screen.blit(kk_img, [300, 200])
         screen.blit(kk_img,kk_rct)#4>8_5
         pg.display.update()
         tmr += 1        
